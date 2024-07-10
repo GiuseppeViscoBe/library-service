fd0bf08bf42561f2eca445288d1971246857e9db
Giuseppe Visco <g.visco.ita@be-tse.it>
2024-07-10 10:27:40 +0200

    Update books.controller.js

--- routes/books.controller.js
+++ routes/books.controller.js
@@ -1,6 +1,6 @@
 const bookModel = require("./../models/books.model");
 
-const getBookList = async (req, res) => {
+const getBookList = async (req, res, next) => {
   try {
     const result = await bookModel.getAllBooks();
 
@@ -21,7 +21,6 @@ const getBookByTitleQuery = async (req, res, next) => {
 
     const result = await bookModel.getBookByTitle(title);
 
-    
     if (!result) {
       res.status(404);
       throw Error("No books found");
@@ -29,36 +28,65 @@ const getBookByTitleQuery = async (req, res, next) => {
 
     res.status(200).json(result);
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
-const addBook = async (req, res) => {
+const addBook = async (req, res, next) => {
   try {
     const book = req.body;
+
+    const existingBook = await bookModel.getBookByTitle(book.title);
+
+    if (existingBook) {
+      res.status(409);
+      throw Error("Book already present in the library");
+    }
 
     const result = await bookModel.addBook(book);
 
     res.status(200).json(result);
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteBookByTitle = async (req, res) => {
+const deleteBookByTitle = async (req, res, next) => {
   try {
     const { title } = req.body;
+
+    const existingBook = await bookModel.getBookByTitle(title);
+
+    if (!existingBook) {
+      res.status(404);
+      throw Error("No book with this title present in the library");
+    }
+
     const result = bookModel.deleteBookByTitle(title);
 
     res.status(200).json(result);
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 };
 
-const updateBookByTitle = async (req, res) => {
+const updateBookByTitle = async (req, res, next) => {
   try {
     const { title, quantityAvailable } = req.body;
+
+    const existingBook = await bookModel.getBookByTitle(title);
+
+    if (!existingBook) {
+      res.status(404);
+      throw Error("No book with this title present in the library");
+    }
+
     const result = bookModel.updateBookByTitle(title, quantityAvailable);
 
     res.status(200).json(result);
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
